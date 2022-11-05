28d59627df2e832b5287249da184dd4e80db2276
pwn-rs <tpapachristos@protonmail.com>
2022-11-05 20:31:40 +0200

    move encrypt function to utils

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ mod utils;
 
 
 use clap::Parser;
-use libaes::{ Cipher };
-use passwords::PasswordGenerator;
 // use std::{io::Cursor, path::PathBuf};
 // use image::io::Reader as ImageReader;
 
@@ -12,7 +10,7 @@ use passwords::PasswordGenerator;
 
 
 use cli::{Cli, Modes};
-
+use utils::*;
 
 fn main() {
     let cli = Cli::parse();
@@ -29,32 +27,3 @@ fn main() {
         Modes::Decode(_) => { todo!() }
     }
 }
-
-
-fn encrypt_message(password: String) {
-    let _pg = PasswordGenerator {
-        length: 16,
-        numbers: true,
-        lowercase_letters: true,
-        uppercase_letters: true,
-        symbols: true,
-        spaces: true,
-        exclude_similar_characters: false,
-        strict: true,
-    };
-
-    // Hash the password with BLAKE3
-    // basically the hashed_key is the password
-    let hashed_key = blake3::hash(password.as_bytes());
-    
-    let plaintext = b"A plaintext";
-    let iv = b"This is 16 bytes";
-
-    // Create cipher instance from the hashed_key (password)
-    let cipher = Cipher::new_256(hashed_key.as_bytes());
-
-    let _encrypted = cipher.cbc_encrypt(iv, plaintext);
-
-    println!("HASHED KEY: {}", hashed_key.to_string());
-}
-
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,37 @@
 use std::path::Path;
-
+use libaes::{ Cipher };
+use passwords::PasswordGenerator;
 use image::{DynamicImage, open};
 
 #[allow(dead_code)]
 pub fn file_as_dynamic_image(filename: String) -> DynamicImage {
     let img = open(Path::new(&filename)).unwrap();
     img
+}
+
+pub fn encrypt_message(password: String) {
+    let _pg = PasswordGenerator {
+        length: 16,
+        numbers: true,
+        lowercase_letters: true,
+        uppercase_letters: true,
+        symbols: true,
+        spaces: true,
+        exclude_similar_characters: false,
+        strict: true,
+    };
+
+    // Hash the password with BLAKE3
+    // basically the hashed_key is the password
+    let hashed_key = blake3::hash(password.as_bytes());
+    
+    let plaintext = b"A plaintext";
+    let iv = b"This is 16 bytes";
+
+    // Create cipher instance from the hashed_key (password)
+    let cipher = Cipher::new_256(hashed_key.as_bytes());
+
+    let _encrypted = cipher.cbc_encrypt(iv, plaintext);
+
+    println!("HASHED KEY: {}", hashed_key.to_string());
 }
