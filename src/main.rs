5a8cb8d3d97a5b7cf3601a21b923263d011c2fc4
pwn-rs <tpapachristos@protonmail.com>
2023-02-12 20:47:59 +0200

    parsing okay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{ Parser };
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct StegoArgs {
+    #[arg(value_enum)]
+    mode: String,
+    #[clap(short, long, required = true)]
+    file: String,
+    #[clap(short, long)]
+    message: Option<String>
+}
+
 fn main() {
-    println!("init again");
+    let args = StegoArgs::parse();
+
+    println!("mode: {0} , file: {1} , message: {2:?}", args.mode, args.file, args.message);
 }
