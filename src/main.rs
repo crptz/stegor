428bf076e8912271aae71b76ac6ec7dc70e980de
pwn-rs <tpapachristos@protonmail.com>
2023-02-16 22:12:44 +0200

    basic error handling for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{ Parser };
 
-
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -19,6 +18,10 @@ fn main() {
     match args.mode.as_str() {
         "encode" => {
             println!("Encoding");
+
+            image::open(&args.file).expect("Failed to open image");
+
+            args.message.expect("Message argument is required");
         }
         "decode" => {
             println!("Decoding");
