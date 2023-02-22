b81508b9dd4503f0f22b4ac8c8a1d9ffeae1831c
pwn_it <tpapachristos@protonmail.com>
2023-02-22 14:43:57 +0200

    imlpement embed and extract functionality using cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use hips_lib::hips::{find_secret_img, hide_secret_img};
+use image::{DynamicImage, Pixel, GenericImageView, GenericImage, Rgba};
+use image::io::Reader as ImageReader;
+
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -21,31 +24,30 @@ fn main() {
 
     match args.mode.as_str() {
         "embed" => {
-            println!("Encoding");
+            println!("Embedding...");
 
             // image::open(&args.file).expect("Failed to open image");
 
-            // args.message.expect("Message argument is required");
-            let secret = String::from("hiddenimage");
-            let result_img = if let Some(_message) = args.message {
-                hide_secret_img(&args.file, &secret)
-            } else {
-                panic!();
-            };
+            // Open an image from a file
+            let image = ImageReader::open(&args.file).unwrap().decode().unwrap();
 
-            assert!(result_img.is_ok());
+            // Embed the message in the image
+            let modified_image = embed_message_in_image(image, args.message.expect("Message argument is required"));
+
+            // Save the modified image to a file
+            modified_image.save("output.png").unwrap();
+        
         }
         "extract" => {
-            println!("Decoding");
+            println!("Extracting...");
 
-            let result = find_secret_img(&args.file).unwrap();
-            // assert!(result.is_some());
-            match result {
-                Some(value) => {
-                    println!("{}", value);
-                }
-                None => println!("No value is present"),
+            let image = image::open(args.file).unwrap();
+            if let Some(message) = extract_message_from_image(image) {
+                println!("Extracted message: {}", message);
+            } else {
+                println!("No message found in image");
             }
+
         }
         _ => {
             println!("Invalid Mode.")
@@ -53,4 +55,69 @@ fn main() {
     }
 
     // println!("mode: {0} , file: {1} , message: {2:?}", args.mode, args.file, args.message);
+}
+
+
+
+
+fn embed_message_in_image(image: DynamicImage, message: String) -> DynamicImage {
+    // Get the message bytes and length
+    let message_bytes = message.into_bytes();
+    let message_length = message_bytes.len();
+
+    // Create a new image with the same dimensions as the original image
+    let mut new_image = DynamicImage::new_rgba8(image.width(), image.height());
+
+    // Embed the message length in the first pixel of the new image
+    let first_pixel = Rgba([message_length as u8, 0, 0, 255]);
+    new_image.put_pixel(0, 0, first_pixel);
+
+    // Iterate over the pixels in the original image and embed the message bytes in the new image
+    let mut byte_index = 0;
+    for (x, y, pixel) in image.pixels() {
+        // Skip the first pixel
+        if x == 0 && y == 0 {
+            continue;
+        }
+
+        // Get the original pixel color and embed the message byte in the red channel
+        let mut new_pixel = pixel.to_rgba();
+        if byte_index < message_length {
+            new_pixel[0] = message_bytes[byte_index];
+            byte_index += 1;
+        }
+
+        // Put the new pixel in the new image
+        new_image.put_pixel(x, y, new_pixel);
+    }
+
+    new_image
+}
+
+fn extract_message_from_image(image: DynamicImage) -> Option<String> {
+    // Get the first pixel in the image
+    let first_pixel = image.get_pixel(0, 0);
+
+    // Extract the length of the message from the red channel of the first pixel
+    let message_length = first_pixel[0] as usize;
+
+    // Initialize a vector to hold the message bytes
+    let mut message_bytes = vec![0u8; message_length];
+
+    // Iterate over the pixels in the image containing the message bytes
+    for (byte_index, (_, _, pixel)) in image.pixels().skip(1).take(message_length).enumerate() {
+        let byte = pixel[0];
+        message_bytes[byte_index] = byte;
+    }
+    
+
+    /*
+    For loop for accesing pixel positions, it may need in the future
+    for (x, y, pixel) in image.pixels().skip(1).take(message_length) {
+
+    }
+     */
+
+    // Convert the message bytes vector to a string
+    String::from_utf8(message_bytes).ok()
 }
