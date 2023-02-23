0f9d305b8d9ddb001e36d40fa19252198753b9d6
pwn_it <tpapachristos@protonmail.com>
2023-02-23 16:28:04 +0200

    fucntion renaming

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), ImageError> {
 
             // Embed the message in the image
             let modified_image =
-                embed_message_in_image(image, args.message.expect("Message argument is required"));
+            embed_message_in_red_ch(image, args.message.expect("Message argument is required"));
 
             // Save the modified image to a file
             modified_image.save("output.png")?;
@@ -30,7 +30,7 @@ fn main() -> Result<(), ImageError> {
 
             let image = image::open(args.file)?;
 
-            if let Some(message) = extract_message_from_image(image) {
+            if let Some(message) = extract_message_from_red_ch(image) {
                 println!("Extracted message: {}", message);
             } else {
                 println!("No message found in image");
@@ -41,7 +41,7 @@ fn main() -> Result<(), ImageError> {
     Ok(())
 }
 
-fn embed_message_in_image(image: DynamicImage, message: String) -> DynamicImage {
+fn embed_message_in_red_ch(image: DynamicImage, message: String) -> DynamicImage {
     // Get the message bytes and length
     let message_bytes = message.into_bytes();
     let message_length = message_bytes.len();
@@ -75,7 +75,7 @@ fn embed_message_in_image(image: DynamicImage, message: String) -> DynamicImage 
     new_image
 }
 
-fn extract_message_from_image(image: DynamicImage) -> Option<String> {
+fn extract_message_from_red_ch(image: DynamicImage) -> Option<String> {
     // Get the first pixel in the image
     let first_pixel = image.get_pixel(0, 0);
 
